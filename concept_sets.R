2c21694a04838a0becb91a6fad1e0ede3989df4b
Florian Katsch <florian.katsch@dhp.lbg.ac.at>
2025-11-17 16:27:02 +0100

    add MUW specific concepts to concept_sets.R

--- concept_sets.R
+++ concept_sets.R
@@ -18,6 +18,8 @@ diabetesCS <- querySql(
     )
   )
 names(diabetesCS) <- tolower(names(diabetesCS))  # MUW: since oracle returns upper case column names
+diabetesCS <- rbind(diabetesCS, data.frame(concept_id = c(201820,37018196))) # MUW (Diabetes mellitus, Prediabetes)
+
 
 #Inflammatory Bowel Disease Concept Set
 ibdCS <- querySql(
@@ -25,7 +27,8 @@ ibdCS <- querySql(
   render(
     sqlDiseaseDescendants, 
     databaseSchema = databaseSchema, 
-    concepts = c(201606,81893,194684)
+    concepts = c(201606,81893,194684,
+                 4074815) # MUW (Inflammatory bowel disease)
     )
   )
 names(ibdCS) <- tolower(names(ibdCS))  # MUW: since oracle returns upper case column names
@@ -36,10 +39,12 @@ bcCS <- querySql(
   render(
     sqlDiseaseDescendants, 
     databaseSchema = databaseSchema, 
-    concepts = c(4112853)
+    concepts = c(4112853, 
+                 81250) # MUW ("Carcinoma in situ of breast"), subsumes other concepts in use, e.g.: 601142,4116071, 609066
     )
   )
 names(bcCS) <- tolower(names(bcCS))  # MUW: since oracle returns upper case column names
+bcCS <- rbind(bcCS, data.frame(concept_id = c(759932,36712724,36684818,759933,36712725,765123)))  # MUW: outdated concepts that are still in use at our databases
 
 #Lung cancer Concept Set
 lcCS <- querySql(
